12632b0860f3fcacc96b721a7e69ec3a8414de58
吉超 <1049759078@qq.com>
2018-09-13 19:20:47 +0800

    Update models.py

--- apps/account/models.py
+++ apps/account/models.py
@@ -7,7 +7,8 @@ class User(AbstractUser):
     用户扩展表
     """
     nickname = models.CharField(max_length=10, null=True, blank=True, verbose_name='昵称' ,help_text='昵称')
-
+    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name="手机号", help_text="手机号")
+    
     def __str__(self):
         return self.username
 
